bd2d6e90e1be462f3a7fa0ec76bd79b6400d9727
xyzowiec <lukasz.szalkiewicz@gmail.com>
2023-04-04 23:41:30 +0200

    feat: edit main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,4 @@
 resource "google_storage_bucket" "my_bucket" {
   name     = "dareit-vm-tf-ci-22222220"
   location = "us-central1"
-
-  tags = ["dareit"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        managed_by_terraform = "true"
-      }
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
 }
