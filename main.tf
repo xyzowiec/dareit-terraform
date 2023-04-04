4a6e1ae815a6d2991002179757184be39f034543
xyzowiec <lukasz.szalkiewicz@gmail.com>
2023-04-04 22:58:39 +0200

    feat: edit main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,17 @@
-resource "google_storage_bucket" "dareit-vm-ci-222222" {
-  name         = "dareit-vm-tf-ci-2222222"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
+resource "google_storage_bucket" "new_bucket" {
+  name     = "dareit-vm-tf-ci-2222222"
+  location = "US"
 
-  tags = ["dareit"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        managed_by_terraform = "true"
-      }
-    }
+  versioning {
+    enabled = true
   }
 
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
+  lifecycle {
+    rule {
+      action    = "Delete"
+      condition {
+        age = 30
+      }
     }
   }
 }
